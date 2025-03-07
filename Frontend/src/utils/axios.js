5fa0ee1a61e0ad1bcfd083ae4860923e8c21632d
Sreerag <sreeragrajan07@gmail.com>
2025-03-07 18:35:24 +0530

    updated

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://employee-management-system-793m.vercel.app/api",
     withCredentials: true,
 })
