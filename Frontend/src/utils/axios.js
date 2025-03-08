f6caff6a2e3ddf9413b949e70828f9ff7d3536b8
Sreerag <sreeragrajan07@gmail.com>
2025-03-08 15:54:37 +0530

    updated

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,9 +1,7 @@
-import axios from "axios"
-
-const apiUrl = import.meta.env.VITE_API_URL;
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: apiUrl,
+    baseURL: "https://employee-management-system-leed.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     },
